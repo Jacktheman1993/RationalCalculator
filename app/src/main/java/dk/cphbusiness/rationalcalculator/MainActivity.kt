eff876a62b0cff85cded1fba84e9cc3150ff9c2d
Jacktheman1993 <32671071+Jacktheman1993@users.noreply.github.com>
2019-02-11 09:06:57 +0100

    test

--- app/src/main/java/dk/cphbusiness/rationalcalculator/MainActivity.kt
+++ app/src/main/java/dk/cphbusiness/rationalcalculator/MainActivity.kt
@@ -8,11 +8,14 @@ import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    val calculator = Calculator();
+    lateinit var calculator: Calculator
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        if(savedInstanceState == null) calculator = Calculator()
+        else calculator =
+            savedInstanceState.getSerializable("calculator") as Calculator? ?: Calculator()
         setContentView(R.layout.activity_main)
         // val eb = findViewById<Button>(R.id.enterButton)
         enterButton.setOnClickListener {
@@ -20,31 +23,37 @@ class MainActivity : AppCompatActivity() {
             if (number == null) {
                 val top = calculator.stack1
                 calculator.push(top)
-                }
+            }
             else calculator.enter(number)
             updateStack()
             editText2.setText("")
-            }
         }
+        updateStack()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putSerializable("calculator", calculator)
+    }
 
     override fun onPause() {
         super.onPause()
-      }
+    }
 
     fun checkInput() {
         val number = editText2.text.toString().toLongOrNull()
         if (number != null) {
             calculator.enter(number)
             editText2.setText("")
-            }
         }
+    }
 
     fun updateStack() {
         stack4.setText(calculator.stack4.text)
         stack3.setText(calculator.stack3.text)
         stack2.setText(calculator.stack2.text)
         stack1.setText(calculator.stack1.text)
-        }
+    }
 
     fun doOperation(view: View) {
         checkInput()
@@ -54,7 +63,7 @@ class MainActivity : AppCompatActivity() {
             timesButton -> calculator.times()
             divButton -> calculator.div()
             // divButton -> calculator = calculator.nyDiv()
-            }
-        updateStack()
         }
+        updateStack()
     }
+}
